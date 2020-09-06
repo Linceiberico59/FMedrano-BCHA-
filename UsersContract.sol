39f3eea9fcf007fab9369bd46b03f3d0fe0d316d
Francisco José Medrano <66084146+Linceiberico59@users.noreply.github.com>
2020-09-06 14:19:22 +0200

    Add files via upload

---
+++ UsersContract.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+contract UsersContract {
+
+    struct User {
+        string name;
+        string surName;
+    }
+
+    mapping(address => User) private users;
+
+    function join(string name, string surName) public {
+        User storage user = users [msg.sender];
+        user.name = name;
+        user.surName = surName;
+    }
+
+    function getUser(address addr) public view returns (string, string) {
+        User memory user = users[addr];
+        return(user.name, user.surName);
+    }
+   
+}
